8724242234d4c5ecfdde1d5f2496c8317dbf13be
Clement raczek <clement.raczek@labom2iformation.fr>
2025-11-06 10:01:26 +0100

    add files

---
+++ read/models/Movie.py
@@ -0,0 +1,20 @@
+class Movie:
+
+    _id_counter = 31  
+
+    def __init__(self, titre: str, annee_production: int, genre: str, age_limite: int):
+        self.id = Movie._id_counter
+        Movie._id_counter += 1
+
+        # Attributs d'instance
+        self.titre = titre
+        self.annee_production = annee_production
+        self.genre = genre
+        self.age_limite = age_limite
+
+    def __str__(self):
+
+        return (f"🎬 {self.titre} ({self.annee_production})\n"
+                f"   Genre : {self.genre}\n"
+                f"   Âge minimum : {self.age_limite} ans\n"
+                f"   ID : {self.id}")
